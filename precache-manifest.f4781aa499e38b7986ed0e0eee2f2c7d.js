6ca5fe009432a6f96fa5cc2e071a121e60156228
Jinaka <jinaka.ugochukwu@gmail.com>
2018-11-29 12:22:13 +0000

    Updates

---
+++ precache-manifest.f4781aa499e38b7986ed0e0eee2f2c7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Jinaka/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "df3686aa611e9aa8b598",
+    "url": "/Jinaka/static/js/runtime~main.df3686aa.js"
+  },
+  {
+    "revision": "ef4091b5d809c6d9e616",
+    "url": "/Jinaka/static/js/main.ef4091b5.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/Jinaka/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "ef4091b5d809c6d9e616",
+    "url": "/Jinaka/static/css/main.8f22c26a.chunk.css"
+  },
+  {
+    "revision": "edf2f3b7fdf2630dccdb6cc917f8e916",
+    "url": "/Jinaka/index.html"
+  }
+];
